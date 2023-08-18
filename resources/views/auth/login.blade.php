b59cd40706b00f78192c4eab6a5baebdb5c04f8a
Dayan Betancourt <dkbetancourt@gmail.com>
2023-08-18 14:37:48 -0400

    style :art: Design Dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,38 +11,52 @@
                 {{ session('status') }}
             </div>
         @endif
+        
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Iniciar sesión
+            </h1>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+            <form method="POST" action="{{ route('login') }}" class="space-y-4 md:space-y-6">
+                @csrf
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
+                <div>
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input type="email" name="email" id="email" :value="old('email')" placeholder="name@company.com" required autofocus />
+                </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+                <div>
+                    <x-label for="password" value="{{ __('Password') }}" />
+                    <x-input type="password" name="password" id="password" placeholder="••••••••" required />
+                </div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                <div class="flex items-center justify-between">
+                    <div class="flex items-start">
+                        <div class="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
+                        </div>
+                        <div class="ml-3 text-sm">
+                            <label for="remember" class="text-gray-500 dark:text-gray-300">{{ __('Remember me') }}</label>
+                        </div>
+                    </div>
+                    @if (Route::has('password.request'))
+                        <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-4">
+                <x-button>
                     {{ __('Log in') }}
                 </x-button>
-            </div>
-        </form>
+
+                @if (Route::has('register'))
+                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Don’t have an account yet? 
+                        <a href="{{ route('register') }}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                    </p>
+                @endif
+            </form>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block w-full p-2.5 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg sm:text-sm focus:ring-primary-600 focus:border-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500']) !!}>
