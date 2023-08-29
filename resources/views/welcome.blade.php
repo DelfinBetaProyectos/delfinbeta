b478dae63b91d7eecaa89ae3b66cc99d537adfb7
Dayan Betancourt <dkbetancourt@gmail.com>
2023-08-29 10:38:36 -0400

    Google Tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -543,6 +543,16 @@
 
     @livewireScripts
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5FZYDWTH61"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-5FZYDWTH61');
+    </script>
+
     <script>
         (function () {
             'use strict';
